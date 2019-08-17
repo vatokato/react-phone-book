4c285e999876cbb021421d79c4ed208250495b8e
RomanOt <vatokato@gmail.com>
2019-08-17 22:34:00 +0500

    app2

---
+++ build/precache-manifest.86ae17cb1c73ce65345364641efd7b17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92ff98b1bd90bfd3037390ae31374445",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8631f2d30f114ffd90ac",
+    "url": "./static/css/main.464a3761.chunk.css"
+  },
+  {
+    "revision": "51ccad8c292e41ee2702",
+    "url": "./static/js/2.7d6ade0e.chunk.js"
+  },
+  {
+    "revision": "8631f2d30f114ffd90ac",
+    "url": "./static/js/main.a42c2604.chunk.js"
+  },
+  {
+    "revision": "c1b4d132412a0085d743",
+    "url": "./static/js/runtime~main.5a955be1.js"
+  }
+]);
